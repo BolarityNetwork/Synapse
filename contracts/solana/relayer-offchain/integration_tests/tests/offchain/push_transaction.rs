54ad1da21abd34cba2173724e957ed41377bacf9
sulijia <984115358@qq.com>
2025-01-07 21:16:54 +0800

    add relayer offchain project for ncn reach consensus

---
+++ contracts/solana/relayer-offchain/integration_tests/tests/offchain/push_transaction.rs
@@ -0,0 +1,13 @@
+mod push_transaction{
+    use crate::{
+        fixtures::{
+            // test_builder::TestBuilder, tip_router_client::assert_tip_router_error, TestError,
+            TestResult,
+        },
+        // helpers::ballot_box::serialized_ballot_box_account,
+    };
+    #[tokio::test]
+    async fn test_push_transaction_ok() -> TestResult<()> {
+        Ok(())
+    }
+}
