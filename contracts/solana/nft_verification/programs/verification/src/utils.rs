95004657e59c0eaa10fabac05d77502e536d6533
sulijia <984115358@qq.com>
2025-03-11 15:22:52 +0800

    Remove nft veification project,update idl

--- contracts/solana/nft_verification/programs/verification/src/utils.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::Pubkey;
-
-/// Converts an Ethereum address (20 bytes) to a Solana Pubkey (32 bytes)
-/// by padding with zeros
-pub fn ethereum_address_to_pubkey(eth_address: &[u8; 20]) -> Pubkey {
-    let mut bytes = [0u8; 32];
-    bytes[0..20].copy_from_slice(eth_address);
-    Pubkey::new_from_array(bytes)
-}
